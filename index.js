e1a6d1759ac9ffdb9d5f9d099b374a0a0bdc1daa
Travis Storie <storie25@outlook.com>
2022-08-27 12:10:35 -0700

    Finished assignment

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // DEPENDENCIES
-const express = require("express");
+const express = require("express")
 const mongoose = require('mongoose')
 
 // CONFIGURATION
-require("dotenv").config();
-const PORT = process.env.PORT;
-const app = express();
+require("dotenv").config()
+const PORT = process.env.PORT
+const app = express()
 
 // MIDDLEWARE
 app.use(express.json())
@@ -13,16 +13,16 @@ app.use(express.json())
 // ROUTES
 app.get("/", (req, res) => {
     res.send("Hello World!");
-  });
+  })
 
 // Books
-const booksController = require("./controllers/books_controller.js");
-app.use("/books", booksController);
+const booksController = require("./controllers/books-controller");
+app.use("/books", booksController)
 
 // LISTEN
 app.listen(PORT, () => {
     console.log("listening on port", PORT);
-  });
+  })
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
