541ae8df703b360ef41b00d2557bcc353cab9b36
Travis Storie <storie25@outlook.com>
2022-08-27 10:52:58 -0700

    Second half of lesson

--- controllers/books-controller.js
+++ controllers/books-controller.js
@@ -1,76 +1,82 @@
-//dependecies
 const express = require("express");
 const books = express.Router();
 const Book = require("../models/book.js");
-const bookSeed = require("../models/seed-books.js");
 
-//GET /books
-//returns all books
-books.get("/", async (req, res) => {
-    try {
-        const foundBooks = await Book.find();
-        res.status(200).json(foundBooks);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-//GET /seed
-//add 3 books to the DB
-books.get("/seed", async (req, res) => {
-    try {
-        await Book.insertMany(bookSeed);
-        res.status(200).json({
-            message: "Seed Successful"
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-//PUT /books.:id
-//update book by ID
-books.put("/:id", async (req, res) => {
-    try {
-        const foundBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.status(200).json(foundBook);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-//DELETE /books/:id
-//delete book by id
-books.delete("/:id", async (req, res) => {
-    try {
-        await Book.findByIdAndDelete(req.params.id);
-        res.status(200).json({ message: `Successfuly Deleted Book at ID ${req.params.id}` });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-//POST /books
-//add new book
-books.post("/", async (req, res) => {
-    try {
-        const newBook = await Book.create(req.body);
-        res.status(200).json(newBook);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-//GET /books/:id
-//get book by ID
-books.get("/:id", async (req, res) => {
-    try {
-        const foundBook = await Book.findById(req.params.id);
-        res.status(200).json(foundBook);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+books.get('/seed', (req, res) => {
+    Book.insertMany([{
+        "title": "The Shinobi Initiative",
+        "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
+        "year": 2014,
+        "quantity": 10,
+        "imageURL": "https://imgur.com/LEqsHy5.jpeg"
+      },
+      {
+        "title": "Tess the Wonder Dog",
+        "description": "The tale of a dog who gets super powers",
+        "year": 2007,
+        "quantity": 3,
+        "imageURL": "https://imgur.com/cEJmGKV.jpg"
+      },
+      {
+        "title": "The Annals of Arathrae",
+        "description": "This anthology tells the intertwined narratives of six fairy tales.",
+        "year": 2016,
+        "quantity": 8,
+        "imageURL": "https://imgur.com/VGyUtrr.jpeg"
+      },
+      {
+        "title": "Wâˆ€RP",
+        "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+        "year": 2010,
+        "quantity": 4,
+        "imageURL": "https://imgur.com/qYLKtPH.jpeg"
+      }])
+        .then(res.status(200).json({
+            message: 'Seed successful'
+        }))
+        .catch(res.status(400).json({
+            message: 'Seed unsuccessful'
+        }))
 })
 
-//export
+//index
+books.get("/", (req, res) => {
+    Book.find()
+    .then(foundBooks => {
+        res.json(foundBooks)
+    })
+    .catch(err => {
+        if (err && err.name == 'ValidationError') {
+            let message = 'Validation Error: '
+            for (var field in err.errors) {
+                message += `${field} was ${err.errors[field].value}. ${err.errors[field].message}\n`
+            }
+            res.json({ message })
+        }
+        else {
+            res.json('error404')
+        }
+    })
+})
+
+// Show 1
+books.get('/:id', (req, res) => {
+    Book.findById(req.params.id)
+    .then(foundBooks => {
+        res.json(foundBooks)
+        })
+        .catch(err => {
+            if (err && err.name == 'ValidationError') {
+                let message = 'Validation Error: '
+                for (var field in err.errors) {
+                    message += `${field} was ${err.errors[field].value}. ${err.errors[field].message}\n`
+                }
+                res.json({ message })
+            }
+            else {
+                res.json('error404')
+            }
+        })
+    })
+
 module.exports = books
--- models/book.js
+++ models/book.js
@@ -1,16 +1,14 @@
-//dependencies
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
-//schema
 const bookSchema = new Schema({
-    title: {type: String, required: true},
-    description: {type: String, default: "No Description"},
-    year: {type: Number, required: true},
-    quantity: {type: Number, default: 0},
-    imageURL: {type: String, default: "http://placekitten.com/400/400"}
-});
+    title: { type: String, required: true},
+    description: { type: String },
+    year: { type: Number },
+    quantity: { type: Number },
+    imageURL: { type: String, default: 'https://placekitten.com/500/500' },
+  }
+)
 
-//model and export
-const Book = mongoose.model("Book", bookSchema);
+const Book = mongoose.model('Book', bookSchema)
 module.exports = Book
